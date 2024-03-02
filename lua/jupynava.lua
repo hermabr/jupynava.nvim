3755b68f952c342e70306ce87ac1be53a0720e34
Herman Brunborg <herman@brunborg.com>
2024-03-02 14:37:39 -0800

    move keybinds out of setup function

--- lua/jupynava.lua
+++ lua/jupynava.lua
@@ -94,14 +94,47 @@ function SendToTerm(mode, ...)
     end
 end
 
+function EvaluateCodeBlock(skipToNextCodeBlock)
+    local original_cursor = vim.api.nvim_win_get_cursor(0)
+    local mode = vim.api.nvim_get_mode().mode
+    if mode == 'i' or mode == 'v' or mode == 'V' or mode == '' then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+    end
+    local current_line = vim.api.nvim_get_current_line()
+    if current_line == "# +" or current_line == "# -" then
+        vim.cmd('normal! j')
+    end
+    local pattern = '# [-+]$'
+    local start_row = vim.fn.search(pattern, 'bnW') + 1
+    local end_row = vim.fn.search(pattern, 'nW') - 1
+    if end_row == -1 then end_row = vim.fn.line('$') end
+    if start_row > end_row then return end
+
+    -- Retrieve the lines of code within the block
+    local lines = vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, false)
+
+    SendToTerm('direct', lines)
+
+    if skipToNextCodeBlock and end_row == vim.fn.line('$') then
+        vim.api.nvim_buf_set_lines(0, -1, -1, false, {"# +"})
+        vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
+    end
+    if skipToNextCodeBlock then
+        vim.api.nvim_win_set_cursor(0, {end_row + 2, 0})
+    end
+end
+
 function M.setup(user_config)
     M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
-
-    vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>lua ToggleTerm()<CR>', {noremap = true, silent = true})
-
-    vim.api.nvim_set_keymap('n', 'ss', "<cmd>lua SendToTerm('direct', vim.fn.getline('.'))<CR>", {silent = true, noremap = true})
-    vim.api.nvim_set_keymap('n', 's', "<cmd>set opfunc=v:lua.SendToTerm<CR>g@", {silent = true, noremap = true})
-    vim.api.nvim_set_keymap('v', 's', ":<C-u>lua SendToTerm('v')<CR>", {silent = true, noremap = true})
 end
+
+vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>lua ToggleTerm()<CR>', {noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('n', 'ss', "<cmd>lua SendToTerm('direct', vim.fn.getline('.'))<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', 's', "<cmd>set opfunc=v:lua.SendToTerm<CR>g@", {silent = true, noremap = true})
+vim.api.nvim_set_keymap('v', 's', ":<C-u>lua SendToTerm('v')<CR>", {silent = true, noremap = true})
+
+vim.api.nvim_set_keymap('n', '<C-Enter>', "<cmd>lua EvaluateCodeBlock(false)<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<S-Enter>', "<cmd>lua EvaluateCodeBlock(true)<CR>", {silent = true, noremap = true})
 
 return M
