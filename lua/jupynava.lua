284248d930fe741a4d2abd64ae151999130540da
Herman Brunborg <herman@brunborg.com>
2024-04-01 05:53:12 -0700

    refactor terminal handling

--- lua/jupynava.lua
+++ lua/jupynava.lua
@@ -1,56 +1,40 @@
-local M = {}
+if _G.loaded_sendtoterm then
+    return
+end
+_G.loaded_sendtoterm = true
 
-M.config = {
-    term_width = 73,
-    begin_str_send_to_term = "\x1b[200~",
-    end_str_send_to_term = "\x1b[201~\r\r\r",
-    newline_str_send_to_term = vim.fn.has("win32") == 1 and "\r\n" or "\n",
-}
+local nl = vim.fn.has("win32") == 1 and "\r\n" or "\n"
 
-local term_buf_id = nil
-local term_win_id = nil
-
-M.ShowTerm = function()
-    local current_win = vim.api.nvim_get_current_win()
-    local total_width = vim.api.nvim_get_option("columns")
-    local split_cmd = total_width < (2 * M.config.term_width) and "botright split" or "botright vsplit"
-
-    vim.cmd(split_cmd)
-    if split_cmd == "botright split" then
-        local total_height = vim.api.nvim_get_option("lines")
-        local desired_height = math.floor(total_height / 2)
-        vim.api.nvim_win_set_height(0, desired_height)
-    else
-        vim.api.nvim_win_set_width(0, M.config.term_width)
+local function sendHere(term_type)
+    term_type = term_type or 'default'
+    local bufnr = vim.api.nvim_get_current_buf()
+    if vim.bo[bufnr].buftype ~= 'terminal' then
+        print('This buffer is not a terminal.')
+        return
     end
 
-    if term_buf_id and vim.api.nvim_buf_is_valid(term_buf_id) then
-        vim.api.nvim_win_set_buf(0, term_buf_id)
-        term_win_id = vim.api.nvim_get_current_win()
-    else
-        vim.cmd("terminal ipython")
-        term_buf_id = vim.api.nvim_get_current_buf()
-        term_win_id = vim.api.nvim_get_current_win()
-        vim.api.nvim_win_set_option(term_win_id, 'number', false)
-        vim.api.nvim_win_set_option(term_win_id, 'relativenumber', false)
-        vim.api.nvim_win_set_option(term_win_id, 'signcolumn', 'no')
-        vim.api.nvim_command("norm G")
-    end
-    vim.api.nvim_set_current_win(current_win)
+    _G.send_target = vim.tbl_extend("force", {
+        term_id = bufnr,
+        send = function(lines)
+            local self = _G.send_target
+            local line
+            if #lines > 1 then
+                line = self.begin .. table.concat(lines, self.newline) .. self['end']
+            else
+                line = lines[1] .. nl
+            end
+            local channel_id = vim.api.nvim_buf_get_option(self.term_id, 'channel')
+            vim.api.nvim_chan_send(channel_id, line)
+            if vim.v.count1 > 1 then
+                vim.cmd('sleep 100m')
+            end
+        end
+    }, {begin = "\x1b[200~", ['end'] = "\x1b[201~\r\r\r", newline = nl})
 end
 
-M.HideTerm = function()
-    vim.api.nvim_win_hide(term_win_id)
-    term_win_id = nil
-end
-
-function ToggleTerm()
-    if term_win_id and vim.api.nvim_win_is_valid(term_win_id) then
-        M.HideTerm()
-    else
-        M.ShowTerm()
-    end
-end
+vim.api.nvim_create_user_command('SendHere', function()
+    sendHere()
+end, {})
 
 local function captureText(mode)
     local marks = mode:match("[vV]") and {"'<", "'>"} or {"'[", "']"}
@@ -68,44 +52,20 @@ local function captureText(mode)
     return lines
 end
 
-function SendHere()
-    local buf = vim.api.nvim_get_current_buf()
-    if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
-        term_buf_id = buf
-        term_win_id = vim.api.nvim_get_current_win()
-    else
-        print("Not in a terminal buffer")
-    end
-end
-
-function SendToTerm(mode, ...)
-    if not term_win_id then
-        M.ShowTerm()
+local function send(mode, ...)
+    if not _G.send_target then
+        print('Target terminal not set. Run :SendHere or :SendTo first.')
+        return
     end
 
     local lines
     if mode == 'direct' then
-        if type(...) == 'table' then
-            lines = ...
-        else
-            lines = {...}
-        end
+        lines = {...}
     else
         lines = captureText(mode)
     end
 
-    local line
-    if #lines > 1 then
-        line = M.config.begin_str_send_to_term .. table.concat(lines, M.config.newline_str_send_to_term) .. M.config.end_str_send_to_term
-    else
-        line = lines[1] .. M.config.newline_str_send_to_term
-    end
-
-    local channel_id = vim.api.nvim_buf_get_option(term_buf_id, 'channel')
-    vim.api.nvim_chan_send(channel_id, line)
-    if vim.v.count1 > 1 then
-        vim.cmd('sleep 100m')
-    end
+    _G.send_target.send(lines)
 end
 
 function EvaluateCodeBlock(skipToNextCodeBlock)
@@ -122,34 +82,128 @@ function EvaluateCodeBlock(skipToNextCodeBlock)
     local start_row = vim.fn.search(pattern, 'bnW') + 1
     local end_row = vim.fn.search(pattern, 'nW') - 1
     if end_row == -1 then end_row = vim.fn.line('$') end
-    if start_row > end_row then return end
+    if start_row >= end_row then return end
 
+    -- Retrieve the lines of code within the block
     local lines = vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, false)
 
-    SendToTerm('direct', lines)
+    if not _G.send_target then
+        print('Target terminal not set. Run :SendHere or :SendTo first.')
+        return
+    end
+
+    _G.send_target.send(lines)
 
     if skipToNextCodeBlock and end_row == vim.fn.line('$') then
         vim.api.nvim_buf_set_lines(0, -1, -1, false, {"# +"})
         vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
-    elseif end_row == vim.fn.line('$') - 1 then
-        vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
     end
     if skipToNextCodeBlock then
-        vim.api.nvim_win_set_cursor(0, {end_row + 2, 0})
+        vim.defer_fn(function() vim.api.nvim_win_set_cursor(0, {end_row + 2, 0}) end, 10)
+    else
+        vim.defer_fn(function() vim.api.nvim_win_set_cursor(0, original_cursor) end, 10)
     end
 end
 
-function M.setup(user_config)
-    M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
+_G.send = send -- Export send function to global scope for key mapping command to work
+
+local function set_python_keymaps()
+  if vim.bo.filetype == 'python' then
+    vim.keymap.set({'i', 'n', 'v'}, '<C-Enter>', function() EvaluateCodeBlock(false) end, {noremap = true, silent = true})
+    vim.keymap.set({'i', 'n', 'v'}, '<S-Enter>', function() EvaluateCodeBlock(true) end, {noremap = true, silent = true})
+    function JumpUpSection()
+        local cur_row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+        local pattern = '# [-+]$'
+        local found_row = vim.fn.search(pattern, 'bnW')
+        if found_row == 0 then
+            vim.api.nvim_win_set_cursor(0, {1, 0})
+            return
+        end
+        if cur_row == found_row then
+            vim.cmd('normal! k')
+            local new_row = vim.fn.search(pattern, 'bnW')
+            vim.api.nvim_win_set_cursor(0, {found_row, 0})
+        else
+            vim.api.nvim_win_set_cursor(0, {found_row, 0})
+        end
+        vim.cmd('normal! zz')
+        vim.cmd('nohlsearch')
+    end
+    vim.api.nvim_set_keymap('n', '[n', ':lua JumpUpSection()<CR>', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('n', ']n', '/# [-+]$<CR><CMD>noh<CR>zz', {noremap = true, silent = true})
+
+    vim.api.nvim_set_keymap('n', 'ss', "<cmd>lua _G.send('direct', vim.fn.getline('.'))<CR>", {silent = true, noremap = true})
+    vim.api.nvim_set_keymap('n', 's', "<cmd>set opfunc=v:lua._G.send<CR>g@", {silent = true, noremap = true})
+    vim.api.nvim_set_keymap('v', 's', ":<C-u>lua _G.send('v')<CR>", {silent = true, noremap = true})
+  end
+end
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "FileType"}, { pattern = "*.py", callback = set_python_keymaps, })
+
+-- Disable mapping if set
+if vim.g.send_disable_mapping then
+    return
 end
 
-vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>lua ToggleTerm()<CR>', {noremap = true, silent = true})
+function reloadPlugin()
+    _G.loaded_sendtoterm = false
+    dofile("/Users/herman/dev/git/nvim-send-to-term/plugin/send-to-term.lua")
+    print("Plugin reloade")
+end
 
-vim.api.nvim_set_keymap('n', 'ss', "<cmd>lua SendToTerm('direct', vim.fn.getline('.'))<CR>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', 's', "<cmd>set opfunc=v:lua.SendToTerm<CR>g@", {silent = true, noremap = true})
-vim.api.nvim_set_keymap('v', 's', ":<C-u>lua SendToTerm('v')<CR>", {silent = true, noremap = true})
+-- Key binding for reloading the plugin
+vim.api.nvim_set_keymap('n', 'S', "<cmd>lua reloadPlugin()<CR>", {noremap = true, silent = true})
 
-vim.keymap.set({'i', 'n', 'v'}, '<C-Enter>', function() EvaluateCodeBlock(false) end, {noremap = true, silent = true})
-vim.keymap.set({'i', 'n', 'v'}, '<S-Enter>', function() EvaluateCodeBlock(true) end, {noremap = true, silent = true})
+local term_buf = nil
+local term_win = nil
+function TermToggle(width)
+    local current_win = vim.api.nvim_get_current_win()
+    local total_width = vim.api.nvim_get_option("columns")
+    local total_height = vim.api.nvim_get_option("lines")
+    local desired_height = math.floor(total_height / 2) -- Calculate half the height of the current window
+    local split_cmd = total_width < (2 * width) and "botright split" or "botright vsplit"
+    -- If the terminal window is visible, hide it
+    if term_win and vim.api.nvim_win_is_valid(term_win) then
+        vim.api.nvim_win_hide(term_win)
+        term_win = nil
+    else
+        -- Otherwise, open or show the terminal window without focusing it
+        local win_exists = term_win and vim.api.nvim_win_is_valid(term_win)
+        if win_exists then
+            -- Show the terminal without focusing
+            vim.api.nvim_win_hide(term_win) -- Hide first to reset the state
+            vim.cmd(split_cmd) -- This will create a new split or focus the existing one based on total width
+            if split_cmd == "botright split" then
+                vim.api.nvim_win_set_height(0, desired_height)
+            else
+                vim.api.nvim_win_set_width(0, width)
+            end
+            vim.api.nvim_set_current_win(current_win) -- Return focus back to the original window
+        else
+            -- Create a new split for the terminal
+            vim.cmd(split_cmd)
+            if split_cmd == "botright split" then
+                vim.api.nvim_win_set_height(0, desired_height)
+            else
+                vim.api.nvim_win_set_width(0, width)
+            end
+            local new_win = vim.api.nvim_get_current_win()
+            if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+                -- Switch to the existing terminal buffer
+                vim.api.nvim_win_set_buf(new_win, term_buf)
+            else
+                -- Create a new terminal buffer
+                vim.cmd("terminal ipython")
+                term_buf = vim.api.nvim_get_current_buf()
+                vim.api.nvim_win_set_option(new_win, 'number', false)
+                vim.api.nvim_win_set_option(new_win, 'relativenumber', false)
+                vim.api.nvim_win_set_option(new_win, 'signcolumn', 'no')
+                vim.api.nvim_command("SendHere")
+                vim.api.nvim_command("norm G")
+            end
+            term_win = new_win
+            vim.api.nvim_set_current_win(current_win) -- Do not focus the new terminal window
+        end
+    end
+end
 
-return M
+vim.api.nvim_set_keymap('n', '<leader>s', ':lua TermToggle(73)<CR>', {noremap = true, silent = true})
